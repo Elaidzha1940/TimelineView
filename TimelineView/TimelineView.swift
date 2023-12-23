70b9f005b2e3b577b0fcffb554f8e03cc2fe1687
Elaidzha1940 <eli080293@gmail.com>
2023-12-23 15:10:10 +0300

    TimelineView

---
+++ TimelineView/TimelineView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: TimelineView
+//  File: TimelineView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 23.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    TimelineView()
+}
---
+++ TimelineView/TimelineViewApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: TimelineView
+//  File: TimelineViewApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 23.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct TimelineViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TimelineView()
+        }
+    }
+}
