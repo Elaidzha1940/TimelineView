1ce53feb56ec57f52419a5184e3bc68218efc6d9
Elaidzha1940 <eli080293@gmail.com>
2023-12-24 13:45:37 +0300

    Update TimelineView

--- TimelineView/TimelineView.swift
+++ TimelineView/TimelineView.swift
@@ -13,7 +13,19 @@ struct TimelineView: View {
     var body: some View {
         
         VStack {
-            
+            TimelineView(.animation) { context in
+                Text("\(context.date)")
+                
+                let seconds = Calendar.current.component(.second, from: context.date)
+                Text("\(seconds)")
+                
+                Ellipse()
+                    .frame(
+                        width: seconds < 10 ? 50 : seconds < 30 ? 200 : 300,
+                        height: 100
+                    )
+                    .animation(.bouncy, value: seconds)
+            }
         }
     }
 }
